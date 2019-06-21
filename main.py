58d3889039b09d3d6abf7e06798ddc9f5a6c98c3
alessandro <alessandro@yuneecresearch.com>
2019-06-21 15:23:17 +0200

    fetching all links working in all dirs

--- main.py
+++ main.py
@@ -4,82 +4,76 @@ import http.client
 from html.parser import HTMLParser
 import sys
 
-# link = '192.168.42.1'
-link = '192.168.42.1'
+# host = '192.168.42.1'
+host = '192.168.42.1'
+run_continuously = True
+desired_extensions = ['jpg', 'jpeg'] # Capitalization ignored
 
 class MyHTMLParser(HTMLParser):
     def __init__(self):
-        # self.parsing_link = False
         self.rel_links = []
         HTMLParser.__init__(self)
 
     def handle_starttag(self, tag, attrs):
         if tag == 'a':
-            print("Encountered a start tag:", tag)
-            # self.parsing_link = True
+            # print("Encountered a start tag:", tag)
             if attrs[0][0] == 'href':
                 if attrs[0][1] == '../':
+                    # Ignore link to previous site
                     return
 
-                # full_link = link + )
-                # print(full_link)
             self.rel_links.append('/' + str(attrs[0][1]))
 
     def handle_endtag(self, tag):
-        # if tag == 'a':
-        print("Encountered an end tag :", tag)
-        #     self.parsing_link = False
+        if tag == 'a':
+            # print("Encountered an end tag :", tag)
+            pass
 
     def handle_data(self, data):
-        # if self.parsing_link:
-        print("Encountered some data  :", data)
+        # print("Encountered some data  :", data)
+        pass
 
 def get_all_links(host, directory):
-    conn = http.client.HTTPConnection(link)
+    conn = http.client.HTTPConnection(host)
     conn.request("GET", directory)
     r = conn.getresponse()
     print(r.status, r.reason)
 
     # Check HTTP response (Code 200 means OK)
     if r.status != 200:
-        # TODO: Handle gracefully
-        print("Error connecting to camera")
-        exit()
+        return None
 
     data = r.read()
     conn.close()
 
     parser = MyHTMLParser()
-    parser.feed(data)
+    parser.feed(str(data))
     return parser.rel_links
 
-## Get all directories from index site
-# directories = get_all_links(link, "/226E50HD/")
-directories = get_all_links(link, "/")
-print("Directories:")
-print(directories)
+while(run_continuously):
+    ## Get all directories from index site
+    # directories = get_all_links(link, "/226E50HD/")
+    directories = get_all_links(host, "/")
 
-for d in directories:
-    print("Fetching directory " + d)
-    links = get_all_links(d)
-    print(links)
+    if directories == None:
+        # TODO: sleep for one second or something
+        continue
 
+    print("Found directories:")
+    print(directories)
+    print("")
 
+    for d in directories:
+        print("Fetching directory " + d)
+        links = get_all_links(host, d)
+        print(links)
+        for l in links:
+            # 
 
-    # conn = http.client.HTTPConnection(d)
-    # conn.request("GET","/")
-    # r1 = conn.getresponse()
-    # print(r1.status, r1.reason)
+            # TODO: Does this need additional slashes?
+            print("Downloading image " + host + d + l)
+            # TODO: Download images to specified directory 
 
-    # if r1.status != 200:
-    #     print("Error connecting to camera")
-    #     exit()
+    # TODO: Sleep for some seconds
 
-    # data = r1.read()
-    # conn.close()
-
-    # parser2 = MyHTMLParser()
-    # parser.feed(data)
-
-    # print("Files: ")
-    # print(parser.links)
+    exit()
